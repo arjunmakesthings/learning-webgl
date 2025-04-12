8cd102ac9c287a3ff595d470a4840c69fc35061e
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-04-12 20:45:36 +0530

    achieved: clear and set background

--- sketches/250405_setup-sketch/sketch.js
+++ sketches/250405_setup-sketch/sketch.js
@@ -1,18 +1,25 @@
 //ref: https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
 
-main(); //this is the main function, where the main shader code sits. 
+let gl; 
 
-function main() {
+setup(); //this is the main function, where the main shader code sits. 
+
+function setup() {
   const canvas = document.querySelector("#main-canvas"); //assign 'canvas' to refer to the html-canvas.
-  const gl = canvas.getContext("webgl"); //getContext tells the canvas what kind of drawing environment one wants to use.
+  gl = canvas.getContext("webgl"); //getContext tells the canvas what kind of drawing environment one 
 
-  /*fail-safe:*/
+  /* fail-safe: */
   if (!gl) {
     alert("your browser or machine does not support webgl. sadly, this sketch cannot run on it."); 
     return; 
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearColor (1.0,0.0,0.0,1.0); //sets clearColor rgba values.  
+  gl.clear (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //clear takes boolean values, so you can apparently pass multiple values into this function with an 'or' operator; meaning "clear this or this, or both if both are present." 
+
+  draw(); 
+}
+
+function draw(){
+//
 }
