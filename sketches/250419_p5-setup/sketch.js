c253b6c0e58269af71b125adf3d0cb0e471cc4a1
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-04-20 19:18:45 +0530

    interim p5 commit.

---
+++ sketches/250419_p5-setup/sketch.js
@@ -0,0 +1,23 @@
+//realised that writing native web-gl shaders is extremely painful. so, resorted to using p5 instead (since it has a bunch of wrappers to make writing shaders a little less tedious.)
+
+let shader_1; 
+
+function preload(){
+  shader_1 = loadShader("vert.glsl", "frag.glsl"); //load shader into p5. 
+}
+
+function setup() {
+
+  let canvas = createCanvas(640, 480, WEBGL);
+  canvas.parent("canvas-container");
+
+  shader(shader_1);
+  noStroke();
+}
+
+function draw(){
+  clear();
+
+  //run shader: 
+  rect(0,0, width, height);
+}
