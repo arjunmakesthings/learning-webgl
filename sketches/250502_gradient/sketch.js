da423519e9b3ac5e83e10933cd8e4e9f62054ad9
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-05-02 11:11:20 +0530

    add: new folder.

---
+++ sketches/250502_gradient/sketch.js
@@ -0,0 +1,27 @@
+/*
+realised that writing native web-gl shaders is extremely painful. so, resorted to using p5 instead (since it has a bunch of wrappers to make writing shaders a little less tedious.)
+
+tutorial followed: https://www.youtube.com/watch?v=3mfvZ-mdtZQ
+*/
+
+let shader_1; 
+
+function preload(){
+  shader_1 = loadShader("vert.glsl", "frag.glsl"); //load shader into p5. 
+}
+
+function setup() {
+
+  let canvas = createCanvas(640, 480, WEBGL);
+  canvas.parent("canvas-container");
+
+  shader(shader_1);
+  noStroke();
+}
+
+function draw(){
+  clear();
+
+  //run shader:
+  ellipse(0,0, width, height);
+}
