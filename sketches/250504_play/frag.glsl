9b5c26d3fb5586224fe28183de2908b70a465f43
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-05-04 14:34:03 +0530

    achieved: pixel control.

--- sketches/250504_play/frag.glsl
+++ sketches/250504_play/frag.glsl
@@ -5,6 +5,17 @@ varying vec2 pos; //this is what comes from the vert shader. each pixel on the s
 uniform vec2 u_resolution; //takes value from sketch. has to be changed / updated if sketch must be responsive. 
 
 void main() {
+  vec2 pixel_coord = pos * u_resolution; //convert uv to pixel space. 
 
-  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0); //:output. 
+  float w = u_resolution.x; 
+
+  if (pixel_coord.x < w/2.0){
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
+  }else{
+    gl_FragColor = vec4 (0.0, 0.0, 1.0, 1.0); 
+  }
+
+
+
+  // gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0); //:output. 
 }
